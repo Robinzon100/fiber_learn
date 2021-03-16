a34a11476f0f81b4f5ccad074ac95450b945456e
Robinzon100 <robinzon.sokhashvili@gmail.com>
2021-03-16 04:55:47 +0400

    :boom: initial

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/:id", func(c *fiber.Ctx) error {
+		// c.SendString()
+
+		return c.JSON(fiber.Map{
+			"name": c.Params("id"),
+			"age":  20,
+		})
+	})
+
+	app.Listen(":3000")
+}
